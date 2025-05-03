26470f9ae8a08a34cbd9bb14d38dbb9e518b2c47
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-05-03 23:49:56 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Counter from './components/Counter'
 import Todos from './components/Todos'
 import MealsAPI from './components/MealsAPI'
 import Calculator from './components/Calculator'
+import ToggleBackgroundColor from './components/ToggleBackgroundColor'
+import HiddenSearchBar from './components/HiddenSearchBar'
+import Testimonials from './components/Testimonials'
+import Accordion from './components/Accordion'
+import { accordionData } from "./utils/content"
+import FormValidation from './components/FormValidation'
 
 
 function App() {
@@ -10,12 +16,29 @@ function App() {
 
   return (
     <>
-      {/*  <Counter />  // Project 1 - Counter 
+      {/*  
+          <Counter />  // Project 1 - Counter 
           <Todos />    //  Project-2 - Todos
           <MealsAPI /> // Project-3 - Meals API
           <Calculator /> // Project - 4 Calculator 
+          <ToggleBackgroundColor /> // Project - 5 ToogleBackgroundColor
+          <HiddenSearchBar />   // Project - 6 Hidden Search Feature
+          <Testimonials /> // project - 7 Testimonials Card
+       
+          //Project -8 Accordian Card
+          <div>
+      <div className='accordion'>
+        {accordionData.map(({title, content}) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+     </div>
+
+     //Project 9 
+
       */}
       
+     <FormValidation />
     </>
   )
 }
---
+++ src/components/HiddenSearchBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import "../index.css"
+
+const HiddenSearchBar = () => {
+      const [showInput, setShowInput] = useState(false);
+      const [bgColor, setBgColor] = useState("white");
+
+      const handeleClick = (e) => {
+        setBgColor("#1a1a1a");
+        if (e.target.className === "container") {
+          setShowInput(false);
+          setBgColor("#fff");
+        }
+      }
+
+  return (
+    <section className='container' stlye={{ backgroundColor: bgColor }}
+    onClick={handeleClick}>
+     {showInput ? (
+        <input type='text' placeholder='Search...' />
+     ): ( <FaSearch onClick={() => setShowInput(true)}/>
+     )}
+    </section>
+  )
+}
+
+export default HiddenSearchBar
